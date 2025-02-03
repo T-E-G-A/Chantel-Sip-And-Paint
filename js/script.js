f684bd58f698736666c69de8febabb43c622e13f
Oviasogie Edward <104275659+T-E-G-A@users.noreply.github.com>
2025-02-03 11:41:01 +0000

    reupload of chantel-sip-and-paint

---
+++ js/script.js
@@ -0,0 +1,91 @@
+
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () =>{
+   menu.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
+};
+
+window.onscroll = () =>{
+   menu.classList.remove('fa-times');
+   navbar.classList.remove('active');
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+   const loadMoreBtn = document.querySelector('.load-more');
+   if (loadMoreBtn) {
+       loadMoreBtn.onclick = () => {
+           let boxes = [...document.querySelectorAll('.packages .box-container .box')];
+           for (var i = currentItem; i < currentItem + 3; i++) {
+               boxes[i].style.display = 'inline-block';
+           }
+           currentItem += 3;
+           if (currentItem >= boxes.length) {
+               loadMoreBtn.style.display = 'none';
+           }
+       };
+   }
+});
+
+
+var swiper = new Swiper(".home-slider", {
+   loop:true,
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+});
+
+var swiper = new Swiper(".reviews-slider", {
+   grabCursor:true,
+   loop:true,
+   autoHeight:true,
+   spaceBetween: 20,
+   breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      700: {
+        slidesPerView: 2,
+      },
+      1000: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+let loadMoreBtn = document.querySelector('.packages .load-more .btn');
+let currentItem = 3;
+
+loadMoreBtn.onclick = () =>{
+   let boxes = [...document.querySelectorAll('.packages .box-container .box')];
+   for (var i = currentItem; i < currentItem + 3; i++){
+      boxes[i].style.display = 'inline-block';
+   };
+   currentItem += 3;
+   if(currentItem >= boxes.length){
+      loadMoreBtn.style.display = 'none';
+   }
+}
+
+function sendMail(event) {
+   event.preventDefault(); // prevent form submission
+   let parms = {
+     name: document.getElementById("name").value,
+     email: document.getElementById("email").value,
+     subject: document.getElementById("subject").value,
+     Phone: document.getElementById("number").value, 
+     message: document.getElementById("message").value,
+   }
+
+   emailjs.send("service_7mkcare", "template_2v1xbnm", parms)
+     .then(function(response) {
+      showCustomAlert(`Thank you, We have received your messaged`);
+       console.log("SUCCESS!", response);
+     }, function(error) {
+       showCustomAlert("Email failed to send. Please try again later.");
+       console.log("FAILED...", error);
+     });
+ }
+
